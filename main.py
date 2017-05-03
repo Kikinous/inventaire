b98c61f49afe5d9b7df01f2c01071462e6974d0b
Kikinous <julienborghetti@gmail.com>
2017-05-03 15:15:53 +0200

    InitialCommit

---
+++ main.py
@@ -0,0 +1,34 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.tabbedpanel import TabbedPanel
+
+class InventaireGui(Widget):
+    pass
+
+class TabbedPanelLotB(TabbedPanel):
+
+    def coucou(self, stockPB):
+        print ('coucou' + str(stockPB.value))
+
+    def mns(self, ProgressBarIntra, ProgressBarExtra):
+        if  ProgressBarIntra.value + ProgressBarExtra.value - 1 <= ProgressBarIntra.max:
+            ProgressBarIntra.value = ProgressBarIntra.value + ProgressBarExtra.value - 1
+            ProgressBarExtra.value = 0
+        elif ProgressBarIntra.value + ProgressBarExtra.value - 1 > ProgressBarIntra.max:
+            ProgressBarExtra.value = ProgressBarExtra.value - 1
+            ProgressBarIntra.value = ProgressBarIntra.max
+    def pls(self, ProgressBarIntra, ProgressBarExtra):
+        if  ProgressBarIntra.value + ProgressBarExtra.value + 1 > ProgressBarIntra.max:
+            ProgressBarIntra.value = ProgressBarIntra.max
+            ProgressBarExtra.value = ProgressBarExtra.value + 1
+        elif ProgressBarIntra.value + ProgressBarExtra.value + 1 <= ProgressBarIntra.max:
+            ProgressBarExtra.value = 0
+            ProgressBarIntra.value = ProgressBarIntra.value + 1
+
+class InventaireApp(App):
+    def build(self):
+        return InventaireGui()
+
+
+if __name__ == '__main__':
+    InventaireApp().run()
